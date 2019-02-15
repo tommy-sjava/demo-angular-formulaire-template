c619d4e54cd0c5de3bec73a7bc71e5f4d0831dc2
amicius <admin@amicius.fr>
2019-02-15 16:49:29 +0100

    a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AvisComponent } from './avis/avis.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AvisComponent
   ],
   imports: [
     NgbModule,
